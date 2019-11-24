f7896786671a962b80c9e65a7826aac067a76348
fluke097 <58104371+fluke097@users.noreply.github.com>
2019-11-24 16:43:29 +0700

    Create week2-9.go

---
+++ week2-9.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("10 is of type %T\n", 10)
+
+	fmt.Printf("10 is fo tapy %T\n", string(10))
+
+	fmt.Printf("10 is of tape %T\n", float64(10))
+}
