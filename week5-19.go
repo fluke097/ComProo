0b5edd83bf8cfff038b0ccf70e224b18b3a79df6
fluke097 <58104371+fluke097@users.noreply.github.com>
2019-12-07 12:40:59 +0700

    Create week5-19.go

---
+++ week5-19.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 1
+	switch number {
+	case 0:
+		fmt.Println("zero")
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("four")
+	default:
+		fmt.Println("unknow")
+	}
+}
