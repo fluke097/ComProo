1f4e5a55f2f6c286bfd784df0b208f913cf0af3d
fluke097 <58104371+fluke097@users.noreply.github.com>
2019-12-29 12:49:05 +0700

    Create week8-6.go

---
+++ week8-6.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type I interface {
+}
+
+func desc(i I) {
+	fmt.Printf("%v, %T \n", i, i)
+}
+
+type T1 struct {
+	text string
+}
+
+func (t T1) F() {
+	fmt.Println(t.text)
+}
+
+type T2 struct {
+	number int
+}
+
+func (t T2) F() {
+	fmt.Println(t.number)
+}
+
+func main() {
+	i1 := T1{"Heoll World"}
+	i2 := T2{20}
+	desc(i1)
+	desc(i2)
+}
