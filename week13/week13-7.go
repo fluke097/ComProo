c4198332e029351556c5d59d4ff6e287fc5ce24b
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-16 18:00:42 +0700

    Update week13-7.go

--- week13/week13-7.go
+++ week13/week13-7.go
@@ -7,12 +7,14 @@ import (
 )
 
 func main() {
-	myList := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	myList := []string{"1", "3", "2", "9", "6", "5", "4", "7", "10", "8"}
 
 	fmt.Printf("Before: %v\n", myList)
 	sort.Slice(myList, func(i, j int) bool {
-
 		numA, _ := strconv.Atoi(myList[i])
 		numB, _ := strconv.Atoi(myList[j])
+		return numA < numB
 	})
+
+	fmt.Printf("After: %v\n", myList)
 }
