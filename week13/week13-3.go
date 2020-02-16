5b43bb61e6a96da2bf9992a9ed42a20035bed071
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-16 16:59:22 +0700

    Update week13-3.go

--- week13/week13-3.go
+++ week13/week13-3.go
@@ -9,6 +9,8 @@ func main() {
 	var myDate time.Time
 	if myDate.IsZero() {
 		fmt.Println("No date has been set, %s\n", myDate)
+	}
+
 	myDate = time.Date(2020, time.February, 1, 0, 0, 0, 0, time.utc)
 	if !myDate.IsZero() {
 		fmt.Println("A date has been set, %s\n", myDate)
