6cc58354152ae7338cca46e4fb6b5121237086a2
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-16 17:53:26 +0700

    Update week13-7.go

--- week13/week13-7.go
+++ week13/week13-7.go
@@ -1,6 +1,8 @@
 package main
 
+import "fmt"
 func main() {
-	myList := []string{"1", "2", "3", "4", "5"}
 	myList := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+
+	fmt.Printf("Before: %v\n", myList)
 }
