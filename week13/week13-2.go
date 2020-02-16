2cd66a463245442d712225f65aba6d1cc37ce0ad
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-16 16:59:59 +0700

    Update week13-2.go

--- week13/week13-2.go
+++ week13/week13-2.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
 	"log"
+)
 
 func main() {
 	content, err := ioutil.ReadFile("golangcode.txt")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	text := string(content)
+	fmt.Println(text)
 }
