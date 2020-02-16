91c7b90ca3c9710e96a4d07a5146e0491cd699ef
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-16 17:19:02 +0700

    Update week13-5.go

--- week13/week13-5.go
+++ week13/week13-5.go
@@ -1,8 +1,16 @@
 package main
 
+import "fmt"
+
 var doOnce sync.Onec
 
 func main() {
 	DoSomething()
 	DoSomething()
+}
+
+func DoSomething() {
+	doOnce.Do(func() {
+		fmt.Println("Run once - first time, loading...")
+	})
 }
