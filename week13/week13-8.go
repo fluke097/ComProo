691f06aefe89eb8a99a846ad4cdd00fd31f7e5e8
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-16 18:16:31 +0700

    Update week13-8.go

--- week13/week13-8.go
+++ week13/week13-8.go
@@ -1,8 +1,11 @@
 package main
 
 type WriteCounter struct {
-	total uint64
+	Total uint64
 }
 
 func (wc *WriteCounter) Write(p []byte) (int, error) {
-
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+}
