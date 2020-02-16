6fcb94c34371b88e40269ce0aeca54ab71960b0c
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-16 17:55:21 +0700

    Update week13-7.go

--- week13/week13-7.go
+++ week13/week13-7.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+	"fmt"
+	"sort"
+)
+
 func main() {
 	myList := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 
 	fmt.Printf("Before: %v\n", myList)
+	sort.Slice(myList, func(i, j int) bool {
+
+	})
 }
