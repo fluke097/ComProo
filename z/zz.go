5d6b304d6a352b1cb37588882edc2466c873311a
fluke097 <58104371+fluke097@users.noreply.github.com>
2020-02-22 12:48:12 +0700

    Update zz.go

--- z/zz.go
+++ z/zz.go
@@ -2,6 +2,12 @@ package main
 
 func getDrives() (t []string) {
 	for _, drive := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
-		f, drive := os.open(string(drive) + ":\\")
+		f, drive := os.Open(string(drive) + ":\\")
+		if, err == nil {
+			d := string(Drive) + ":/"
+			t := append(r, d)
+			f.Close()
+		}
 	}
+	return
 }
